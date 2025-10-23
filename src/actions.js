edb0a9818e7a24f7cc86b765eed06215590831f9
JHenrique <henryque.pc.game@gmail.com>
2025-10-23 18:24:04 -0300

    Utilizando o Hook useFormState

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,9 @@
+"use server";
+
 import { redirect } from "next/navigation";
 import { db } from "./db";
 
 async function deleteTodo(formData) {
-  "use server";
   const id = Number(formData.get("id"));
   await db.todo.delete({ where: { id } });
 
@@ -10,8 +11,6 @@ async function deleteTodo(formData) {
 }
 
 const addTodo = async (formData) => {
-  "use server";
-
   const titulo = formData.get("titulo");
   const descricao = formData.get("descricao");
   const status = "pendente";
@@ -28,9 +27,34 @@ const addTodo = async (formData) => {
 };
 
 const updateFindById = async (id) => {
-  "use server";
   const todo = await db.todo.findFirst({ where: { id: Number(id) } });
   return todo;
 };
 
-export { deleteTodo, addTodo, updateFindById };
+const updateTodo = async (formState, formData) => {
+  const id = Number(formData.get("id"));
+  const titulo = formData.get("titulo");
+  const descricao = formData.get("descricao");
+  //const status = formData.get("status");
+
+  if (titulo.length < 5) {
+    return {
+      errors: "Título precisa ter pelo menos 5 caracteres.",
+    };
+  }
+
+  if (descricao.length < 10) {
+    return {
+      errors: "Descrição precisa ter pelo menos 10 caracteres.",
+    };
+  }
+
+  await db.todo.update({
+    where: { id },
+    data: { titulo, descricao },
+  });
+
+  redirect("/");
+};
+
+export { deleteTodo, addTodo, updateFindById, updateTodo };
--- src/app/todos/[id]/edit/page.js
+++ src/app/todos/[id]/edit/page.js
@@ -1,4 +1,5 @@
 import { updateFindById } from "@/actions";
+import TodoForm from "@/components/TodoForm";
 
 export default async function EditPage({ params }) {
   const { id } = await params;
@@ -7,8 +8,11 @@ export default async function EditPage({ params }) {
   //await new Promise((resolve) => setTimeout(resolve, 1000));
 
   return (
-    <div>
-      <h1>Editando Tarefa: {findById.titulo}</h1>
+    <div className="max-w-md mx-auto mt-10">
+      <h1 className="text-2xl font-bold text-center mb-6">
+        Editando Tarefa: {findById.titulo}
+      </h1>
+      <TodoForm todo={findById} />
     </div>
   );
 }
