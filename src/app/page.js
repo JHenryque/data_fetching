3ab02c05e7188f71586e4ea589486b7b27b0bc4c
JHenrique <henryque.pc.game@gmail.com>
2025-10-23 15:24:38 -0300

    Estilos de projeto

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
+import Button from "@/components/Button";
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const todos = await db.todo.findMany();
@@ -9,16 +11,49 @@ export default async function Home() {
         <h1 className="text-2xl font-bold">Tarefas!</h1>
         <div className="space-y-4 w-full">
           {todos.map((todo) => (
-            <div key={todo.id} className="bg-gray-300 rounded-lg shadow p-4">
-              <h2>{todo.titulo}</h2>
-              <p>{todo.descricao}</p>
-              <p
-                className={
-                  todo.status === "pendente" ? "text-red-500" : "text-green-500"
-                }
-              >
-                {todo.status === "pendente" ? "Pendente" : "Concluida"}
-              </p>
+            <div
+              key={todo.id}
+              className={`bg-gray-100 p-4 rounded-lg shadow ${
+                todo.status === "completa" ? "bg-green-100" : ""
+              }`}
+            >
+              <div className="flex justify-between items-start">
+                <div>
+                  <h2 className="text-xl font-semibold">{todo.titulo}</h2>
+                  <p>{todo.descricao}</p>
+                </div>
+                {/* Formulário para alternar o status */}
+                <div className="flex items-center gap-2">
+                  <p className="italic">completar?</p>
+                  <form action={"toggleTodoStatus"}>
+                    <input type="hidden" name="id" value={todo.id} />
+                  </form>
+                </div>
+              </div>
+
+              <div className="flex space-x-2 mt-3">
+                <Link
+                  href={`/todos/${todo.id}`}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Visualizar
+                </Link>
+                <Link
+                  href={`/todos/${todo.id}/edit/`}
+                  className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Editar
+                </Link>
+                {/* <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                  Excluir
+                </button> */}
+                <form action={"deleteTodo"}>
+                  <input type="hidden" name="id" value={todo.id} />
+                  <Button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                    Excluir
+                  </Button>
+                </form>
+              </div>
             </div>
           ))}
         </div>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-center items-center my-15 gap-5 text-2xl">
-      <Link href="/">Tarefas</Link>
-      <Link href="/todos/create">Inserir nova tarefa</Link>
+    <header className="bg-blue-500 text-slate-100 text-2xl py-10 mb-[65px]">
+      <nav className="container mx-auto flex justify-center gap-5">
+        <Link href="/">Tarefas</Link>
+        <Link href="/todos/create">Inserir nova tarefa</Link>
+      </nav>
     </header>
   );
 }
